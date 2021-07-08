24a504d0a927979995edad6f276013db7aab6b2f
huykevin2002 <giahuy300336@gmail.com>
2021-07-08 22:25:49 +0700

    lan nua ne

--- DSA_Challenge1_Group7/DSA_Challenge1_Group7/DSA_Challenge1_Group7.cpp
+++ DSA_Challenge1_Group7/DSA_Challenge1_Group7/DSA_Challenge1_Group7.cpp
@@ -2,38 +2,40 @@
 #include "Output.h"
 
 int main(int argc, char* argv[]) {
+	if (argc < 3)
+		return 0;
+
 	int foundation_num = 0;
 
 	vector<Score> FA_Score_List = ReadAF("Courses.csv", foundation_num);
-	
+
 	vector<Student> List_Student_Grading = ReadStudent_Grading(FA_Score_List, "Grading.csv");
 	vector<Student> List_Student_Interest = ReadStudent_Interest("Interests.csv");
 
 	sort(List_Student_Grading);
-	sort(List_Student_Interest);	
-	
+	sort(List_Student_Interest);
+
 	HopNhatNhungHocSinhVoTinhBiTrung(List_Student_Grading, foundation_num);
 	HopNhatHocSinh(List_Student_Interest, List_Student_Grading);
 	XoaNhungHocSinhKhongCoThamVong(List_Student_Grading);
-	
+
 	sort_grade(List_Student_Grading);
 
 	unordered_map<wstring, int> major = MajorRead("Majors.csv");
 	SapDatThamVong(List_Student_Grading, major);
-	
+
 	TimNguoiThan(List_Student_Grading, L"SV43");
 
-	//if (strcmp(argv[1], "-all") == 0) {
-	//	NhungNguoiXungDang(List_Student_Grading, "Result.csv");
-	//}
-	//
-	//if (strcmp(argv[1], "-s") == 0) {
-	//	wstring TenNguoiThan(argv[2]);
-	//	TimNguoiThan(List_Student_Grading, TenNguoiThan);
-	//}
-	//
-	//if (strcmp(argv[1], "-m") == 0) {
-	//	wstring TenNhaO(argv[2]);
-	//	writeMajor(List_Student_Grading, TenNhaO);
-	//}
+	wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> converter;
+
+	if (strcmp(argv[1], "-all") == 0)
+		NhungNguoiXungDang(List_Student_Grading, "Result.csv");
+
+
+	if (strcmp(argv[1], "-s") == 0)
+		TimNguoiThan(List_Student_Grading, converter.from_bytes(argv[2]));
+
+
+	if (strcmp(argv[1], "-m") == 0)
+		writeMajor(List_Student_Grading, converter.from_bytes(argv[2]));
 }
